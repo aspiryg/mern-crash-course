a60007c8f54cfc56588d6fa102e05c638370241f
aspiryg <ahmadspierij@gmail.com>
2025-02-17 22:07:06 -0500

    app.js resloving the issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app.jsx";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
